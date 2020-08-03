89dc3b5d530f00b24984274b0d7e3d510387bd06
Nek97 <Nek97@users.noreply.github.com>
2020-08-03 19:43:35 +0000

    deploy: Nek97/patentino-attrezzture-code@e64c7ca5da61c82d02915f55e7f0e90b57d9bee3

---
+++ precache-manifest.c73c19cb0f3ab7c81e24827ef387f5a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e630845ef6ccdfacbed",
+    "url": "/universal-pwa/static/css/main.2800b479.chunk.css"
+  },
+  {
+    "revision": "4e630845ef6ccdfacbed",
+    "url": "/universal-pwa/static/js/main.4e630845.chunk.js"
+  },
+  {
+    "revision": "e739c11a434bb684d682",
+    "url": "/universal-pwa/static/css/1.045ad0c1.chunk.css"
+  },
+  {
+    "revision": "e739c11a434bb684d682",
+    "url": "/universal-pwa/static/js/1.e739c11a.chunk.js"
+  },
+  {
+    "revision": "d74df484760b40d63687",
+    "url": "/universal-pwa/static/js/runtime~main.d74df484.js"
+  },
+  {
+    "revision": "a4749524b08ad2386da82b69e62f612d",
+    "url": "/universal-pwa/static/media/americanExpress.a4749524.png"
+  },
+  {
+    "revision": "1ae6d0b835e8592001d7f738b7ba80ec",
+    "url": "/universal-pwa/static/media/jcb.1ae6d0b8.png"
+  },
+  {
+    "revision": "6fdfef6af53bb294f90038274896ebba",
+    "url": "/universal-pwa/static/media/discover.6fdfef6a.png"
+  },
+  {
+    "revision": "7d41a107c63435c0b78fa2815fadcc79",
+    "url": "/universal-pwa/static/media/about.7d41a107.png"
+  },
+  {
+    "revision": "80502df79042bcd7ec423ef7d712b7ca",
+    "url": "/universal-pwa/index.html"
+  }
+];
