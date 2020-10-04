791087cdb5647870305ce7867b9c35c5053bc21f
Nek97 <Nek97@users.noreply.github.com>
2020-10-04 02:11:29 +0000

    deploy: Nek97/patentino-attrezzture-code@ee09f5fa91af5cb3af60755bfb73ee1e01b1a4bd

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/patentino-attrezzature/precache-manifest.04fca539390e0880cab3e4bed99016d2.js"
+  "/precache-manifest.fce10fedbb71685184d9fbcfd66f3a53.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/patentino-attrezzature/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
